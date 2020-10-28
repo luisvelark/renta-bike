2c0c7ebacd842c7aedc82589b4cf243712c7668d
EstebanGabrielSaavedra <saavedra.esteban.1995@gmail.com>
2020-10-28 18:08:38 -0300

    config database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -34,7 +34,7 @@ class Database extends \CodeIgniter\Database\Config
 		'hostname' => 'localhost',
 		'username' => '',
 		'password' => '',
-		'database' => '',
+		'database' => 'rentabike',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
