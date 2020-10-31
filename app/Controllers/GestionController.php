9d5ba3d2241878932060471437f1828f095b07a7
EstebanGabrielSaavedra <saavedra.esteban.1995@gmail.com>
2020-10-31 18:34:38 -0300

    template

---
+++ app/Controllers/GestionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+
+class GestionController extends BaseController
+{
+    public function index()
+    {
+
+        echo view('index');
+    }
+
+
+
+
+   
+}
