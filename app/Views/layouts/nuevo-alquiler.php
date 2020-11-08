d5dbf289386ed4e45cb1fdeb25ffd2da3951897a
EstebanGabrielSaavedra <saavedra.esteban.1995@gmail.com>
2020-11-08 13:51:05 -0300

    cambios chicos

--- app/Views/layouts/nuevo-alquiler.php
+++ app/Views/layouts/nuevo-alquiler.php
@@ -1,5 +1,8 @@
+<?php
+$hora = new DateTime("now", new DateTimeZone('America/Argentina/Ushuaia'));
+?>
+<h2>Nuevo alquiler</h2>
 <div>
-
   <form class="container">
     <div class="form-group">
 
@@ -8,18 +11,15 @@
         <select class="custom-select">
           <option selected>---</option>
           <?php
-            for ($i = 0; $i < count($datos); $i++) {
-              echo '<option value="' . $datos[$i]['idPuntoED']. '">' . $datos[$i]['direccion'] . '</option>';
+          for ($i = 0; $i < count($datos); $i++) {
+            echo '<option value="' . $datos[$i]['idPuntoED'] . '">' . $datos[$i]['direccion'] . '</option>';
           }
           ?>
-
         </select>
       </div>
-
       <div>
         <label for="exampleInputPassword1">Seleccionar hora de inicio</label>
-        <input type="text" class="form-control" id="exampleInputPassword1">
-        <span>Ejemplo: 20hs - 22hs</span>
+        <input class="form-control" type="time" value=<?php echo '"' . $hora->format('G:i') . '"'?> min=<?php echo '"' . $hora->format('G:i') . '"' ?> max="21:00" step="1">
       </div>
 
       <div>
@@ -33,14 +33,13 @@
           <option value="6">6</option>
         </select>
       </div>
-
       <div>
         <label for="exampleInputPassword1">Dni optativo para devolucion</label>
         <input type="text" class="form-control" id="exampleInputPassword1">
         <span>Ej: 12345678</span>
       </div>
-
-      <button type="submit" class="btn btn-primary">Submit</button>
+          <br>
+      <button type="submit" class="btn btn-primary">¡Alquilar!</button>
     </div>
   </form>
 
