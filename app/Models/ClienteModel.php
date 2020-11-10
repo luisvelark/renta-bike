54f9bf1b58bd9aaf22a8f1993228c9d0ea28a933
EstebanGabrielSaavedra <saavedra.esteban.1995@gmail.com>
2020-11-10 15:21:04 -0300

    credito y multas de cliente con brenda. nueva base

--- app/Models/ClienteModel.php
+++ app/Models/ClienteModel.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Models;
+
 use CodeIgniter\Model;
 use App\Models\MultaModel;
 
-
 class ClienteModel extends Model
 {
+
     protected $table      = 'cliente';
     protected $primaryKey = 'idUsuario';
+
     protected $returnType     = 'array';
     protected $useSoftDeletes = false;
+
     protected $allowedFields = ['puntajeTotal', 'credito', 'suspendido', 'fechaInicioSuspencion', 'fechaFinSuspencion'];
     protected $useTimestamps = false;
     protected $createdField  = 'created_at';
@@ -20,27 +23,23 @@ class ClienteModel extends Model
     protected $validationMessages = [];
     protected $skipValidation     = false;
 
-    public function __construct()
-    {
-        $this->multas = new MultaModel();
-    }
 
     public function obtenerMultas($id)
     {
+        $this->multas = new MultaModel();
         return $this->multas->buscarMultasCliente($id);
     }
-    public function obtenerCredito()
+
+    public function obtenerCredito($id)
     {
-        return 300;
+        $credito = $this->where('idUsuario', $id)->findAll();
+        return $credito[0]['credito'];
     }
-    public function obtenerTodosClientes(){
-        $clientes = $this->findAll();
-        return $clientes;
-    }
-    
-    public function obtenerCliente($dni){ 
+
+    public function obtenerCliente($dni)
+    {
         $query = 'SELECT us.nombre,us.apellido,us.dni,cl.credito FROM cliente as cl INNER JOIN usuario as us 
-        WHERE cl.idUsuario=us.idUsuario AND us.dni'.$dni;
+        WHERE cl.idUsuario=us.idUsuario AND us.dni' . $dni;
         $resultados = $this->db->query($query);
         return $resultados->result();
     }
--- app/Views/layouts/credito-multas-cliente.php
+++ app/Views/layouts/credito-multas-cliente.php
@@ -1,31 +1,37 @@
 <div>
-<h2>Crédito y multas</h2>
-<?php 
-print_r($datos);
-?>
-<!-- <div class="table-responsive">
-    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Position</th>
-          <th>Office</th>
-          <th>Age</th>
-          <th>Start date</th>
-          <th>Salary</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Tiger Nixon</td>
-          <td>System Architect</td>
-          <td>Edinburgh</td>
-          <td>61</td>
-          <td>2011/04/25</td>
-          <td>$320,800</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div> -->
+  <h2>Crédito y multas</h2>
+
+  <?php
+  $multas = $datos['multas'];
+  echo '<h3> Tu crédito actual es: ' . $datos['credito'] . '<br> <br>';
+
+  echo '<div class="card shadow mb-4">
+        <div class="card-header py-3">
+        <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body"> 
+        <div class="table-responsive">
+  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+    <thead>
+      <tr>
+        <th>Monto</th>
+        <th>Fecha de multa</th>
+        <th>Detalle</th>
+        <th>Pagado</th>
+      </tr>
+    </thead>
+    <tbody>';
+  for ($i = 0; $i < count($multas); $i++) {
+    echo '<tr>
+       <td>' . $multas[$i]['monto'] . '</td>' .
+      '<td>' . $multas[$i]['fechaMulta'] . '</td>' .
+      '<td>' . $multas[$i]['detalleMulta'] . '</td>' .
+      '<td>' . $multas[$i]['pagado'] . '</td></tr>';
+  }
+  echo '</tbody>
+  </table>
+</div>
+</div>
+</div>';
+  ?>
 </div>
