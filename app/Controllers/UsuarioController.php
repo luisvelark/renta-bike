381dae43c81a08db37756cbf1508efca3a21ef63
Brenda Uribe <brendantouribe@gmail.com>
2020-11-18 17:08:24 -0300

    alta cliente malir sal

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\Request;
 use App\Models\UsuarioModel;
+use App\Models\ClienteModel;
 
 class UsuarioController extends BaseController
 {
@@ -13,29 +14,42 @@ class UsuarioController extends BaseController
     public function __construct()
     {
         $this->usuario = new UsuarioModel();
+        $this->controladorCliente = new ClienteModel();
+
         helper(['form']);
-       
+
         $this->reglasRegistro = [
             'dni' => [
-                'rules' => 'required|is_unique[usuario.dni]',
+                'rules' => 'exact_length[8]|is_unique[usuario.dni]',
                 'errors' => [
-                    'is_unique' => 'El dni ya se encuentra registrado'
+                    'is_unique' => 'El dni ya se encuentra registrado',
+                    'exact_leght' => 'El dni tiene que tener 8 numeros'
                 ]
             ],
             'rcontraseña' => [
-                'rules' => 'required|matches[contraseña]',
+                'rules' => 'matches[contraseña]',
                 'errors' => [
-                    'required' => 'Las contraseñas no coinciden'
+                    'matches' => 'Las contraseñas no coinciden'
                 ]
-        ],
-        'correo' => [
-            'rules' => 'required|is_unique[usuario.correo]',
-            'errors' => [
-                'is_unique' => 'El correo electronico ya existe'
+            ],
+            'correo' => [
+                'rules' => 'is_unique[usuario.correo]',
+                'errors' => [
+                    'is_unique' => 'El correo electronico ya existe'
+                ]
+            ],
+            'contraseña' => [
+                'rules' => 'min_length[8]',
+                'errors' => [
+                    'min_length' => 'La contraseña tiene que tener como minimo 8 caracteres'
+                ]
+            ], 'cuil' => [
+                'rules' => 'exact_length[11]',
+                'errors' => [
+                    'exact_length' => 'El cuil tiene que tener 11 numeros'
+                ]
             ]
-    ]
-    ];
-
+        ];
     }
 
     public function ingresarAlSistema()
@@ -64,7 +78,7 @@ class UsuarioController extends BaseController
                     echo view('login', $data);
                 }
             } else {
-                $data['error'] = 'El cliente no exite';
+                $data['error'] = 'El cliente no existe';
                 echo view('login', $data);
             }
         } else {
@@ -78,19 +92,48 @@ class UsuarioController extends BaseController
         $user_session->destroy();
         return redirect()->to(base_url());
     }
-    
-    public function registrarUsuario(){
-        if($this->request->getMethod()=="post" && $this->validate($this->reglasRegistro)){
 
-        $this->usuario->save(['dni'=>$this->request->getPost('dni'), 'nombre'=>$this->request->getPost('nombre'),
-        'apellido'=>$this->request->getPost('apellido'),'correo'=>$this->request->getPost('correo'),
-        'telefono'=>$this->request->getPost('telefono'),'domicilio'=>$this->request->getPost('domicilio'),
-        'cuil-cuit'=>$this->request->getPost('cuil'),'fechaNacimiento'=>$this->request->getPost('fecha'),
-        'contraseña'=>$this->request->getPost('contraseña'),'tipo'=>'cliente']);
+    public function registrarUsuario()
+    {
+        if ($this->request->getMethod() == "post" && $this->validate($this->reglasRegistro)) {
+            /* $hash = password_hash($this->request->getPost('contraseña'), PASSWORD_DEFAULT); */
+            $this->usuario->save([
+                'dni' => $this->request->getPost('dni'), 'nombre' => $this->request->getPost('nombre'),
+                'apellido' => $this->request->getPost('apellido'), 'correo' => $this->request->getPost('correo'),
+                'telefono' => $this->request->getPost('telefono'), 'domicilio' => $this->request->getPost('domicilio'),
+                'cuil-cuit' => $this->request->getPost('cuil'), 'fechaNacimiento' => $this->request->getPost('fecha'),
+                'contraseña' => $this->request->getPost('contraseña'), 'tipo' => 'cliente'
+            ]);
+
+            $user = $this->usuario->buscarUsuario($this->request->getPost('correo'));
+
+            $this->controladorCliente->save([
+                'idUsuario'=>19,
+                'puntajeTotal' => 0.0,
+                'credito' => 0.0, 
+                'suspendido' => 1,
+                'fechaInicioSuspencion' => '2020-08-13', 
+                'fechaFinSuspencion' => '2020-09-02'
+            ]);
+            
+            $datosSesion = [
+                'idUsuario' => $user['idUsuario'],
+                'nombre' => $user['nombre'],
+                'apellido' => $user['apellido'],
+            ];
+
+            $sesion = session();
+            $sesion->set($datosSesion);
+            return redirect()->to(base_url() . '/GestionController/indexCliente');
+
+        } else {
+            $data = ['validation' => $this->validator, 'dni'=> $this->request->getPost('dni'),
+            'nombre' => $this->request->getPost('nombre'),
+            'apellido' => $this->request->getPost('apellido'), 'correo' => $this->request->getPost('correo'),
+            'telefono' => $this->request->getPost('telefono'), 'domicilio' => $this->request->getPost('domicilio'),
+            'cuil' => $this->request->getPost('cuil'), 'fecha' => $this->request->getPost('fecha'),
+            'contraseña' => $this->request->getPost('contraseña')];
+            echo view('registrar', $data);
+        }
     }
-    else {
-        $data = ['validation' => $this->validator];
-        echo view('registrar', $data);
-    }
-}
 }
--- app/Views/registrar.php
+++ app/Views/registrar.php
@@ -37,46 +37,47 @@
               <form class="user" method="POST" action="<?php echo base_url(); ?>/UsuarioController/registrarUsuario">
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="text" class="form-control form-control-user" id="nombre" name="nombre" placeholder="Nombre">
+                    <input type="text" class="form-control form-control-user" id="nombre" name="nombre" placeholder="Nombre" autofocus value ="<?php if(isset($nombre)){ echo $nombre; } ?>" required>
                   </div>
                   <div class="col-sm-6">
-                    <input type="text" class="form-control form-control-user" id="apellido" name="apellido" placeholder="Apellido">
+                    <input type="text" class="form-control form-control-user" id="apellido" name="apellido" placeholder="Apellido" value ="<?php if(isset($apellido)){ echo $apellido; } ?>" required>
                   </div>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="number" class="form-control form-control-user" id="dni" name="dni" placeholder="Dni">
+                    <input type="number" class="form-control form-control-user" id="dni" name="dni" placeholder="Dni" value ="<?php if(isset($dni)){ echo $dni; } ?>" required>
                   </div>
                   <div class="col-sm-6">
-                    <input type="text" class="form-control form-control-user" id="cuil" name="cuil" placeholder="Cuil">
+                    <input type="number" class="form-control form-control-user" id="cuil" name="cuil" placeholder="Cuil" value ="<?php if(isset($cuil)){ echo $cuil; } ?>" required>
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="domicilio" name="domicilio" placeholder="Domicilio">
+                  <input type="text" class="form-control form-control-user" id="domicilio" name="domicilio" placeholder="Domicilio" value ="<?php if(isset($domicilio)){ echo $domicilio; } ?>" required>
                 </div>
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="date" class="form-control form-control-user" id="fecha" name="fecha" placeholder="Fecha de nacimiento">
+                    <input type="date" class="form-control form-control-user" id="fecha" name="fecha" placeholder="Fecha de nacimiento" value ="<?php if(isset($fecha)){ echo $fecha; } ?>" required>
                   </div>
                   <div class="col-sm-6">
-                    <input type="number" class="form-control form-control-user" id="telefono" name="telefono" placeholder="Teléfono">
+                    <input type="number" class="form-control form-control-user" id="telefono" name="telefono" placeholder="Teléfono" value ="<?php if(isset($telefono)){ echo $telefono; } ?>" required>
                   </div>
                 </div>
                 <div class="form-group">
-                  <input type="email" class="form-control form-control-user" id="correo" name="correo"placeholder="Correo electrónico">
+                  <input type="email" class="form-control form-control-user" id="correo" name="correo"placeholder="Correo electrónico" value ="<?php if(isset($correo)){ echo $correo; } ?>" required>
                 </div>
                 
                 <div class="form-group row">
                   <div class="col-sm-6 mb-3 mb-sm-0">
-                    <input type="password" class="form-control form-control-user" id="contraseña" name="contraseña" placeholder="Contraseña">
+                    <input type="password" class="form-control form-control-user" id="contraseña" name="contraseña" placeholder="Contraseña" value ="<?php if(isset($contraseña)){ echo $contraseña; } ?>" required>
                   </div>
                   <div class="col-sm-6">
-                    <input type="password" class="form-control form-control-user" id="rcontraseña" name="rcontraseña" placeholder="Repetir contraseña">
+                    <input type="password" class="form-control form-control-user" id="rcontraseña" name="rcontraseña" placeholder="Repetir contraseña" required>
                   </div>
                 </div>
                 <button class="btn btn-primary btn-user btn-block" type="submit">Registrarse</button>
                 </a>
                 <hr>
+
               </form>
               <?php if (isset($validation)) { ?>
                       <div class="alert alert-danger">
@@ -99,6 +100,8 @@
 
   </div>
 
+  
+
   <!-- Bootstrap core JavaScript-->
   <script src="<?php echo base_url('vendor/jquery/jquery.min.js') ?>"></script>
   <script src="<?php echo base_url('vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
@@ -108,6 +111,7 @@
 
   <!-- Custom scripts for all pages-->
   <script src="<?php echo base_url('js/sb-admin-2.js') ?>"></script>
+
 
 </body>
 
