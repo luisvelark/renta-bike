bc35144cc436e177a48184acbf265683639196e2
EstebanGabrielSaavedra <saavedra.esteban.1995@gmail.com>
2020-11-27 13:58:19 -0300

    .

--- app/Views/login.php
+++ app/Views/login.php
@@ -28,8 +28,11 @@
     <div class="row justify-content-center">
 
       <div class="col-xl-10 col-lg-12 col-md-9">
-
+      
         <div class="card o-hidden border-0 shadow-lg my-5">
+         
+        <h2 class="h2 text-gray-900 mb-4" style="margin: 0 auto;">Bienvenido a Renta Bike <i class="fas fa-bicycle"></i></h2>
+        
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
